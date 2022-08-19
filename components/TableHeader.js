8ae744dbcff14194ae0bddf1ee1966fa5eeacfb8
Reo Ito <leo.0210.ito@gmail.com>
2022-08-19 18:26:18 +0900

    リファクタリング

--- components/TableHeader.js
+++ components/TableHeader.js
@@ -1,45 +1,28 @@
 import React from "react";
 
 export default function TableHeader() {
+  const headerColumns = [
+    "No",
+    "販売元",
+    "車種",
+    "乗車人数",
+    "予約日数",
+    "価格",
+  ];
+
   return (
     <thead className=" bg-gray-200">
       <tr>
-        <th
-          scope="col"
-          className="px-6 py-3 text-xs font-bold text-left text-gray-800"
-        >
-          No
-        </th>
-        <th
-          scope="col"
-          className="px-6 py-3 text-xs font-bold text-left text-gray-800"
-        >
-          販売元
-        </th>
-        <th
-          scope="col"
-          className="px-6 py-3 text-xs font-bold text-left text-gray-800"
-        >
-          車種
-        </th>
-        <th
-          scope="col"
-          className="px-6 py-3 text-xs font-bold text-left text-gray-800"
-        >
-          乗車人数
-        </th>
-        <th
-          scope="col"
-          className="px-6 py-3 text-xs font-bold text-right text-gray-800"
-        >
-          予約日数
-        </th>
-        <th
-          scope="col"
-          className="px-6 py-3 text-xs font-bold text-right text-gray-800"
-        >
-          価格
-        </th>
+        {headerColumns.map((col) => {
+          return (
+            <th
+              scope="col"
+              className="px-6 py-3 text-xs font-bold text-left text-gray-800"
+            >
+              {col}
+            </th>
+          );
+        })}
       </tr>
     </thead>
   );
