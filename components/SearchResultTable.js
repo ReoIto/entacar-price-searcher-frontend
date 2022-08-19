486b0dd8f26ab498729ea83cd985100ed5d47bea
Reo Ito <leo.0210.ito@gmail.com>
2022-08-19 22:19:53 +0900

    検索結果のカラムがテーブルからはみ出てしまうためscrollableにする

--- components/SearchResultTable.js
+++ components/SearchResultTable.js
@@ -6,18 +6,16 @@ export default function SearchResultTable({ carList, isLoading }) {
   return (
     <div className="flex flex-col pb-10 rounded-lg border shadow-md">
       <div className="overflow-x-auto">
-        <div className="p-1.5 w-full inline-block align-middle">
+        <div className="w-full inline-block align-middle">
           {isLoading ? (
             <ProgressTableSkeleton />
           ) : (
-            <div className="overflow-hidden border rounded-lg">
-              <table className="min-w-full divide-y divide-gray-200">
-                <TableHeader />
-                <tbody className="divide-y divide-gray-200">
-                  <TableRows carList={carList} />
-                </tbody>
-              </table>
-            </div>
+            <table className="min-w-full divide-y divide-gray-200">
+              <TableHeader />
+              <tbody className="divide-y divide-gray-200">
+                <TableRows carList={carList} />
+              </tbody>
+            </table>
           )}
         </div>
       </div>
