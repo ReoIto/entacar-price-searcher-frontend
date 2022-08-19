5f4cb1c6d789586f1d8938c622473f964435af19
Reo Ito <leo.0210.ito@gmail.com>
2022-08-19 18:30:33 +0900

    不要なコメントアウトを削除

--- components/TableRows.js
+++ components/TableRows.js
@@ -1,30 +1,6 @@
 import React from "react";
 
 export default function TableRows({ carList }) {
-  // const renderCarList = carList.map((item, i) => {
-  //   return (
-  //     <>
-  //       <td className="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap">
-  //         {item.shop_name}
-  //       </td>
-  //       <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
-  //         {item.car_name}
-  //       </td>
-  //       <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
-  //         {item.limit_of_passengers}
-  //       </td>
-  //       <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
-  //         {item.price_title}
-  //       </td>
-  //       <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
-  //         {item.price}
-  //       </td>
-  //       <br />
-  //     </>
-  //   );
-  // });
-
-  // return <tr>{renderCarList}</tr>;
   return (
     <>
       {carList.map((item, index) => {
